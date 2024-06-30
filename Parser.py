9fb2a2cc461b0ff2700e6cbed39555d847ff9786
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2024-06-30 13:11:00 -0400

    Create Parser.py

---
+++ Parser.py
@@ -0,0 +1,43 @@
+class Parser:
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.pos = 0
+        self.ast = self.parse_tokens()
+
+    def parse_tokens(self):
+        ast = []
+        while self.pos < len(self.tokens):
+            token_type, token_value = self.tokens[self.pos]
+            if token_type == 'SECTION':
+                ast.append(self.parse_section())
+            else:
+                self.pos += 1
+        return ast
+
+    def parse_section(self):
+        self.pos += 1
+        section_content = []
+        while self.pos < len(self.tokens):
+            token_type, token_value = self.tokens[self.pos]
+            if token_type == 'ENDSECTION':
+                self.pos += 1
+                break
+            elif token_type == 'BLOCK':
+                section_content.append(self.parse_block())
+            else:
+                self.pos += 1
+        return {'type': 'SECTION', 'content': section_content}
+
+    def parse_block(self):
+        block_type = self.tokens[self.pos][1]
+        self.pos += 1
+        block_content = []
+        while self.pos < len(self.tokens):
+            token_type, token_value = self.tokens[self.pos]
+            if token_type == 'BLOCK':
+                self.pos += 1
+                break
+            else:
+                block_content.append(token_value)
+                self.pos += 1
+        return {'type': block_type, 'content': block_content}
