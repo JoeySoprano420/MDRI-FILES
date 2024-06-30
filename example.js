1a6670aa963d94a6b6348a04e23512bfa5c0afb2
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2024-06-30 12:34:03 -0400

    Create example.js

---
+++ example.js
@@ -0,0 +1,37 @@
+<SECTION>
+** 
+|*| Initialize variables 
+|_|
+    var x = 10;
+    var y = 20;
+** 
+
+|*| Perform addition 
+|_|
+    var sum = x + y;
+** 
+
+|*| Output the result 
+|_| 
+    |=| "The sum of x and y is: " + sum;
+** 
+
+|+| Simulate variable changes 
+|_|
+    x = 15;
+    y = 25;
+** 
+
+|(<::>^<::>)| Perform context-aware action 
+|_|
+    var difference = x - y;
+    |=| "The difference between x and y is: " + difference;
+** 
+
+|-| Request user input 
+|_|
+    var userInput = prompt("Enter a value:");
+    |=| "You entered: " + userInput;
+** 
+
+<END SECTION>
